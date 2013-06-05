9e28a4930125fd4ff6a3b04b54d9228584fca01b
Asmod4n <hendrik-github@beskow.de>
2013-06-05 21:28:22 +0300

    closing Tag for to_html added

--- lib/link_header.rb
+++ lib/link_header.rb
@@ -1,5 +1,5 @@
-require "link_header/version"
-require "strscan"
+require 'link_header/version'
+require 'strscan'
 
 #
 # Represents an HTTP link header of the form described in the draft spec http://tools.ietf.org/id/draft-nottingham-http-link-header-06.txt.
@@ -196,7 +196,7 @@ class LinkHeader
     #   LinkHeader::Link.new(["http://example.com/foo", [["rel", "self"]]]).to_html
     #   #=> '<link href="http://example.com/foo" rel="self">'
     def to_html
-      ([%Q(<link href="#{href}")] + attr_pairs.map{|k, v| "#{k}=\"#{v.gsub(/"/, '\"')}\""}).join(' ')
+      ([%Q(<link href="#{href}")] + attr_pairs.map{|k, v| "#{k}=\"#{v.gsub(/"/, '\"')}\""}).join(' ') + '>'
     end
   end
 end
